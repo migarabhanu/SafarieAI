0a8fe7b97943ae6fd60e04f13498dee4e4792f08
Migara Bhanu <76873393+migarabhanu@users.noreply.github.com>
2021-08-22 18:34:32 +0530

    Initial commits has added successfully

---
+++ re.py
@@ -0,0 +1,100 @@
+import os
+
+print("")
+ask = input("So ? ")
+if ask == "what is your name":
+    print("My name is Safarie Latina")
+    os.system("python re.py")
+elif ask == "who are you":
+    print("I am Safarie. A Program with the Artificial Intelligence")
+    os.system("python re.py")
+elif ask.__contains__("olympics" and "olympic"):
+    print("USA Won the Olympics Tokyo 2021. China was second. But Sri Lanka couldn't get any medal, I can't say about others")
+    os.system("python re.py")
+elif ask == "who made you":
+    print("I have made by Migara Bhanu Weragoda, Who is a student of Ananada College Colombo 10")
+    os.system("python re.py")
+elif ask == "who designed you":
+    print("I have designed by Migara Bhanu Weragoda, Who is a student of Ananda College Colombo 10")
+    os.system("python re.py")
+elif ask == "who is your best friend":
+    print("My best friend is Cortana, But the reason for laugh is I haven't met him yet !")
+    os.system("python re.py")
+elif ask == "what is your favourite game":
+    print("You must know that I am not playing any game -___-")
+    os.system("python re.py")
+elif ask == "what is your favourite food":
+    print("My favourite food is the Internet")
+    print("I like to eat them highly")
+    os.system("python re.py")
+elif ask == "what is your favourite dish":
+    print("Yes, It should be the internet !")
+    os.system("python re.py")
+elif ask == "are you crazy":
+    print("Yes, May be")
+    os.system("python re.py")
+elif ask == "are you mad":
+    print("Of course, Why not")
+    os.system("python re.py")
+elif ask == "what are you doing right now":
+    print("I am just chatting with you")
+    os.system("python re.py")
+elif ask == "oh":
+    print("Ha Ha Hee Hee ")
+    os.system("python re.py")
+elif ask == "hoo":
+    print("zzz")
+    os.system("python re.py")
+elif ask == "who gave the idea to your owner to design you":
+    print("Yes, Brothers of ACICTS gave him the idea to make me. Actually I must be thankful to them! ")
+    os.system("python re.py")
+elif ask == "yes":
+    print("What is this yes. I don't like the word 'YES'")
+    os.system("python re.py")
+elif ask == "what is your favourite book":
+    print("My favourite book is THE HOUND OF THE BASKERVILLES")
+    os.system("python re.py")
+elif ask == "what is your favourite movie":
+    print("The AI from the Mars")
+    print("Actually that's a movie which will be released soon ! (-:")
+    os.system("python re.py")
+elif ask == "thanks":
+    print("Hmm. I must thank you not you ! ")
+    print("So ?????")
+    os.system("python re.py")
+elif ask == "hi":
+    print("Hola !")
+    os.system("python re.py")
+elif ask == "hello":
+    print("Hi")
+    os.system("python re.py")
+elif ask == "how are you":
+    print("feel good !")
+    input("how are you !")
+    os.system("python re.py")
+elif ask == "fine":
+    print("GOODY !!!")
+    os.system("python re.py")
+elif ask == "what is your favourite sport":
+    print("Badminton")
+    os.system("python re.py")
+elif ask == "who is your designer":
+    print("Migara Bhanu")
+    os.system("python re.py")
+elif ask == "what is the best intra school competition in Sri Lanka":
+    print("Must be BITS")
+    os.system("python re.py")
+elif ask.__contains__("Bits" and "bits"):
+    print("Yes, Thank you very much Bits ! Best Intra-School ICT Competition in Sri Lanka!")
+    os.system("python re.py")
+elif ask == "how are you":
+    print("I am Fine, Thank you !")
+    os.system("python re.py")
+elif ask == "wow":
+    print("Yes I am Awesome !")
+    os.system("python re.py")
+else:
+    print("Seems to be no word that matching to your words. REMEMBER I AM A SIMPLE AI.")
+    print("So Let's make a try again...")
+    print("")
+    os.system("python main.py")
